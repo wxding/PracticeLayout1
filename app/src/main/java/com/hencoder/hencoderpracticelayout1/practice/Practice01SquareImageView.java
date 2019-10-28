64fe97ff6f8761babde4ec8af626f76a5de6b5fc
丁伟轩 <wxdingcn@gmail.com>
2019-10-28 10:35:46 +0800

    完成

--- app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -3,12 +3,15 @@ package com.hencoder.hencoderpracticelayout1.practice;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 /**
  * 需要把它写成正方形的 ImageView
  */
 public class Practice01SquareImageView extends ImageView {
+    private static final String TAG = "111";
+
     public Practice01SquareImageView(Context context) {
         super(context);
     }
@@ -27,6 +30,22 @@ public class Practice01SquareImageView extends ImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        Log.e(TAG, "onMeasure: "+getMeasuredHeight()+"  "+getMeasuredWidth() );
+
+        int pWidth = getMeasuredWidth();
+        int pHeight = getMeasuredHeight();
+
+        int width;
+        int height;
+        if (pWidth > pHeight) {
+            width = pHeight;
+            height = pHeight;
+        } else {
+            width=pWidth;
+            height = pWidth;
+        }
+        setMeasuredDimension(width,height);
 
         // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
 
